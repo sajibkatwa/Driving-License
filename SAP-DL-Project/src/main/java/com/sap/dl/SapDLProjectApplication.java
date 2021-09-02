d4704b1d37a8544ab9eb9dc0c8ea3d6558068ddf
slogic <slogic@localhost>
2021-09-02 14:38:38 +0530

    Rename files

--- SAP-DL-Project/src/main/java/com/sap/dl/SapDLProjectApplication.java
+++ SAP-DL-Project/src/main/java/com/sap/dl/SapDLProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SapDlProjectApplication {
+public class SapDLProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SapDlProjectApplication.class, args);
+		SpringApplication.run(SapDLProjectApplication.class, args);
 	}
 
 }
