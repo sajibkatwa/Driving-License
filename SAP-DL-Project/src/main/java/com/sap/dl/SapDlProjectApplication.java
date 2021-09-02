4ddb2906a5e95cae72f395922a78d6260bac8c69
slogic <slogic@localhost>
2021-09-02 13:25:12 +0530

    First Commit

---
+++ SAP-DL-Project/src/main/java/com/sap/dl/SapDlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sap.dl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapDlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapDlProjectApplication.class, args);
+	}
+
+}
---
+++ SAP-DL-Project/src/test/java/com/sap/dl/SapDlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.dl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SapDlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
